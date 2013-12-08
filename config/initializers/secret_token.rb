838e446aeb3d6e8179d64241152f2d601fee6d6c
DeProdigy <alex.hint.ah@gmail.com>
2013-12-07 19:23:46 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RoaR::Application.config.secret_key_base = '383f1d91ba3a24c4a8748e184a995b66a47a7c391804d08de30137ed53384cf7059b9e26a4da6351fe4f4b23be00ad499cf2aae7aa33c3644c4fcc114f74e63f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RoaR::Application.config.session_store :cookie_store, key: '_RoaR_session'
